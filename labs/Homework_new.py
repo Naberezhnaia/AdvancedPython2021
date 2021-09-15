5d5a69a7fc09b6518d10a867fe6cd3a13fa85a36
Naberezhnaia <naberezhnaia.er@phystech.edu>
2021-09-16 00:04:08 +0300

    add Hw1

---
+++ labs/Homework_new.py
@@ -0,0 +1,9 @@
+alphabet = ['à','á','â','ã','ä','å','¸','æ','ç','è','é','ê','ë','ì','í','î','ï','ð','ñ','ò','ó','ô','õ','ö','÷','ø','ù','ú','û','ü','ý','þ','ÿ',' ']
+number = input()
+message = []
+while number != 'End':
+    message.append(alphabet[int(number)])
+    number = input()
+for i in message:
+    print (i, end='')
+
