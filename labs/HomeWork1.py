9bd7f15ab0279a87bfb44bac4ed5feb9488696a4
Naberezhnaia <naberezhnaia.er@phystech.edu>
2021-09-18 18:55:30 +0300

    add HW1

--- labs/HomeWork1.py
+++
@@ -1,9 +0,0 @@
-alphabet = ["à","á","â","ã","ä","å","¸","æ","ç","è","é","ê","ë","ì","í","î","ï","ð","ñ","ò","ó","ô","õ","ö","÷","ø","ù","ú","û","ü","ý","þ","ÿ"," "]
-number = input()
-message = []
-while number != "End":
-    message.append(alphabet[int(number)])
-    number = input()
-for i in message:
-    print (i, end='')
-
